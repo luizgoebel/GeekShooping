b9362e97efabb3acf89baaa0a426226062559103
Luiz Eduardo Goebel <goebel.adm@gmail.com>
2025-10-12 16:54:56 -0300

    Refatora e adiciona métodos ao ProductController
    Refatora os métodos `Get` e `Get(long id)` para retornarem
    tipos mais específicos (`IEnumerable<ProductDto>` e
    `ProductDto`, respectivamente).
    
    Adiciona os métodos `Create`, `Update` e `Delete` ao
    `ProductController`, permitindo criar, atualizar e excluir
    produtos. Inclui validações de entrada e respostas adequadas
    como `BadRequest` e `NotFound` nos novos métodos.

--- GeekShooping.ProductAPI/Controllers/ProductController.cs
+++ GeekShooping.ProductAPI/Controllers/ProductController.cs
@@ -17,18 +17,45 @@ public class ProductController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult> Get()
+    public async Task<ActionResult<IEnumerable<ProductDto>>> Get()
     {
         IEnumerable<ProductDto> products = await _productRepository.FindAll();
         return Ok(products);
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult> Get(long id)
+    public async Task<ActionResult<ProductDto>> Get(long id)
     {
         ProductDto product = await _productRepository.FindById(id);
         if (product == null)
             return NotFound();
         return Ok(product);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ProductDto>> Create([FromBody] ProductDto productDto)
+    {
+        if (productDto == null)
+            return BadRequest();
+        ProductDto product = await _productRepository.Create(productDto);
+        return Ok(product);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult<ProductDto>> Update([FromBody] ProductDto productDto)
+    {
+        if (productDto == null)
+            return BadRequest();
+        ProductDto product = await _productRepository.Update(productDto);
+        return Ok(product);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete(long id)
+    {
+        bool status = await _productRepository.Delete(id);
+        if (!status)
+            return NotFound();
+        return Ok(status);
     }
 }
