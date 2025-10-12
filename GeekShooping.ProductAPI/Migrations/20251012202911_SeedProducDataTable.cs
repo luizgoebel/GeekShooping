97f1edd3ec0a5af49da4fa6ef7698ed6c4c0218a
Luiz Eduardo Goebel <goebel.adm@gmail.com>
2025-10-12 17:30:42 -0300

    Seed inicial da tabela `product` com novos produtos
    Adicionada inserção de dados iniciais (seed data) para a tabela
    `product` no contexto do banco de dados `MySQLContext`. Três
    novos produtos foram incluídos: Camiseta Geek, Caneca Geek e
    Mouse Gamer.
    
    Alterado `MySQLContextModelSnapshot.cs` e `MySQLContext.cs`
    para refletir os dados iniciais utilizando o método `HasData`.
    
    Criada a migração `20251012202911_SeedProducDataTable` para
    inserir e remover os dados iniciais da tabela `product`.

---
+++ GeekShooping.ProductAPI/Migrations/20251012202911_SeedProducDataTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GeekShooping.ProductAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProducDataTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[,]
+                {
+                    { 4L, "Roupas", "Camiseta do GeekShooping", "https://static.geekshooping.com/img/camiseta-geek.png", "Camiseta Geek", 29.90m },
+                    { 5L, "Acessórios", "Caneca personalizada do GeekShooping", "https://static.geekshooping.com/img/caneca-geek.png", "Caneca Geek", 15.50m },
+                    { 6L, "Eletrônicos", "Mouse gamer do GeekShooping", "https://static.geekshooping.com/img/mouse-gamer.png", "Mouse Gamer", 199.90m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 4L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 5L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 6L);
+        }
+    }
+}
--- GeekShooping.ProductAPI/Model/Context/MySQLContext.cs
+++ GeekShooping.ProductAPI/Model/Context/MySQLContext.cs
@@ -8,4 +8,37 @@ public class MySQLContext : DbContext
     public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
 
     public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>().HasData(
+            new Product
+            {
+                Id = 4,
+                Name = "Camiseta Geek",
+                Price = 29.90M,
+                Description = "Camiseta do GeekShooping",
+                CategoryName = "Roupas",
+                ImageUrl = "https://static.geekshooping.com/img/camiseta-geek.png"
+            },
+            new Product
+            {
+                Id = 5,
+                Name = "Caneca Geek",
+                Price = 15.50M,
+                Description = "Caneca personalizada do GeekShooping",
+                CategoryName = "Acessórios",
+                ImageUrl = "https://static.geekshooping.com/img/caneca-geek.png"
+            },
+            new Product
+            {
+                Id = 6,
+                Name = "Mouse Gamer",
+                Price = 199.90M,
+                Description = "Mouse gamer do GeekShooping",
+                CategoryName = "Eletrônicos",
+                ImageUrl = "https://static.geekshooping.com/img/mouse-gamer.png"
+            }
+        );
+    }
 }
