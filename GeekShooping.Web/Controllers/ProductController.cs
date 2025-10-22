986df7cfa39697e21c7f20e7e8276c887a9cec2c
Luiz Eduardo Goebel <goebel.adm@gmail.com>
2025-10-21 22:27:28 -0300

    Implementação de CRUD para produtos
    Adicionados métodos no `ProductController.cs` para criar, atualizar e deletar produtos, com suporte para operações `GET` e `POST`. As views `ProductCreate.cshtml`, `ProductDelete.cshtml`, `ProductIndex.cshtml` e `ProductUpdate.cshtml` foram criadas para suportar as operações de CRUD, permitindo a interação completa com os dados dos produtos através do serviço `IProductService`.

--- GeekShooping.Web/Controllers/ProductController.cs
+++ GeekShooping.Web/Controllers/ProductController.cs
@@ -7,7 +7,7 @@ namespace GeekShooping.Web.Controllers;
 public class ProductController : Controller
 {
     private readonly IProductService _productService;
-    
+
     public ProductController(IProductService productService)
     {
         _productService = productService;
@@ -15,7 +15,66 @@ public class ProductController : Controller
 
     public async Task<IActionResult> ProductIndex()
     {
-       IEnumerable<ProductModel> products = await _productService.FindAllProducts();
+        IEnumerable<ProductModel> products = await _productService.FindAllProducts();
         return View(products ?? []);
+    }
+
+    public async Task<IActionResult> ProductCreate()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ProductCreate(ProductModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            ProductModel response = await _productService.CreateProduct(model);
+            if (response != null)
+                return RedirectToAction(nameof(ProductIndex));
+        }
+
+        return View(model);
+    }
+
+    public async Task<IActionResult> ProductUpdate(long id)
+    {
+        ProductModel model = await _productService.FindProductById(id);
+        if (model != null)
+            return View(model);
+
+        return NotFound();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ProductUpdate(ProductModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            ProductModel response = await _productService.UpdateProduct(model);
+            if (response != null)
+                return RedirectToAction(nameof(ProductIndex));
+        }
+
+        return View(model);
+    }
+
+    public async Task<IActionResult> ProductDelete(long id)
+    {
+        ProductModel model = await _productService.FindProductById(id);
+        if (model != null)
+            return View(model);
+
+        return NotFound();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ProductDelete(ProductModel model)
+    {
+            bool response = await _productService.DeleteProductById(model.Id);
+            if (response)
+                return RedirectToAction(nameof(ProductIndex));
+
+        return View(model);
     }
 }
